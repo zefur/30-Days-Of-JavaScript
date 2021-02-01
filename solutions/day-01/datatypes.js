dfaac0ef7e1b409c7451ccd3d7aaf5d999aa4dca
James Hall <jdhall@live.co.uk>
2021-02-01 01:46:31 +0000

    Day one tasks done

---
+++ solutions/day-01/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof "whats my name?");
+console.log(typeof 9800);
+console.log(typeof false);
+console.log(typeof null);
---
+++ solutions/day-01/variable.js
@@ -0,0 +1,35 @@
+let string = "Hello from home";
+let boolean = true;
+// let undefined;
+let empty = null;
+
+let hello;
+let goodbye;
+let left;
+let right;
+
+let good = "pizza";
+let bad = "peas";
+let high = "mountain";
+let low = "valley";
+
+let firstName = "James";
+let lastName = "Secret";
+let maritalStatus = true;
+let country = "England";
+let age = 32;
+
+console.log(firstName, lastName, maritalStatus, country, age);
+
+let first = "Daniel",
+  last = "Jack",
+  married = false,
+  place = "thailand",
+  years = 16;
+console.log(first, last, married, place, years);
+
+let myAge = 32;
+let yourAge = 37;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
